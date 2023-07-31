0e49fa92be2813d640d17090cc1a6de7199c4cb6
Luis Pellizzon <luis.pellizzon@gmail.com>
2023-07-31 11:58:05 +0100

    Creating customs error handlers as middlewares

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -4,6 +4,10 @@ import asyncHandler from 'express-async-handler'
 // route POSt /api/user/auth
 //@access Public
 const authUser = asyncHandler((req, res) =>{
+
+    res.status(401)
+    throw new Error("Something wrong")
+
     res.status(200).json({
         message: "Auth User"
     })
--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,14 @@ import dotenv from "dotenv"
 dotenv.config()
 const port = process.env.PORT || 5000;
 import userRoutes from "./routes/userRoutes.js"
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 const app = express();
 
 app.use('/api/users', userRoutes)
 app.get('/', (req, res) => res.send("Server is ready!"))
+
+app.use(notFound)
+app.use(errorHandler)
 
 app.listen(port,() => console.log(`Server started on port ${port}`))

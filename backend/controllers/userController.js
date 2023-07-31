d730acb6f64815e31bb1166d71b958a8d97850bd
Luis Pellizzon <luis.pellizzon@gmail.com>
2023-07-31 11:37:55 +0100

    Controllers folder for server logic

---
+++ backend/controllers/userController.js
@@ -0,0 +1,12 @@
+// @desc Auth user/set token
+// route POSt /api/user/auth
+//@access Public
+const authUser = async (req, res) =>{
+    res.status(200).json({
+        message: "Auth User"
+    })
+};
+
+export {
+    authUser
+};

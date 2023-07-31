2dd187bf110048e6a39ec7a46113016b55627497
Luis Pellizzon <luis.pellizzon@gmail.com>
2023-07-31 12:24:20 +0100

    Connecting all our user's function from controllers to the routes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -4,15 +4,50 @@ import asyncHandler from 'express-async-handler'
 // route POSt /api/user/auth
 //@access Public
 const authUser = asyncHandler((req, res) =>{
-
-    res.status(401)
-    throw new Error("Something wrong")
-
     res.status(200).json({
         message: "Auth User"
     })
 });
 
+// @desc Register new user
+// route POST /api/users
+//@access Public
+const registerUser = asyncHandler((req, res) =>{
+    res.status(200).json({
+        message: "Register User"
+    })
+});
+
+// @desc Logout user
+// route POST /api/user/logout
+//@access Public
+const logoutUser = asyncHandler((req, res) =>{
+    res.status(200).json({
+        message: "Logout User"
+    })
+});
+
+// @desc Get user profile
+// route GET /api/users/profile
+//@access Private
+const getUserProfile = asyncHandler((req, res) =>{
+    res.status(200).json({
+        message: " User Profile"
+    })
+});
+
+// @desc Update user profile
+// route PUT /api/users/profile
+//@access Private
+const updateUserProfile = asyncHandler((req, res) =>{
+    res.status(200).json({
+        message: "Update User"
+    })
+});
 export {
-    authUser
+    authUser, 
+    registerUser,
+    logoutUser,
+    getUserProfile,
+    updateUserProfile
 };

3f72c13c4a5c410261368cef2c23a9bfd761f7ae
Luis Pellizzon <luis.pellizzon@gmail.com>
2023-07-31 12:46:23 +0100

    Connecting to MongoDB database through db.js file created inside a config folder

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,9 @@ dotenv.config()
 const port = process.env.PORT || 5000;
 import userRoutes from "./routes/userRoutes.js"
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import connectDB from "./config/db.js";
 
+connectDB();
 const app = express();
 
 app.use('/api/users', userRoutes)

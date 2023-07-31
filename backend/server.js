d76dbdb42cf5dae836e3a513d94deaf61d0bbd83
Luis Pellizzon <luis.pellizzon@gmail.com>
2023-07-31 14:23:22 +0100

    Adding cookie parser

--- backend/server.js
+++ backend/server.js
@@ -5,12 +5,14 @@ const port = process.env.PORT || 5000;
 import userRoutes from "./routes/userRoutes.js"
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import connectDB from "./config/db.js";
+import cookieParser from "cookie-parser";
 
 connectDB();
 const app = express();
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
+app.use(cookieParser())
 
 app.use('/api/users', userRoutes)
 app.get('/', (req, res) => res.send("Server is ready!"))

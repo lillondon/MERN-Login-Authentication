b8fc81fc785c84d34e6b013bc8413c4280e4dbc3
Luis Pellizzon <luis.pellizzon@gmail.com>
2023-08-04 15:11:33 +0100

    User api slice for hitting the endpoints for user functions

---
+++ frontend/src/features/api/usersApiSlice.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-unused-vars */
+import { apiSlice } from "./apiSlice";
+const USERS_URL = "/api/users";
+
+export const userApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    login: builder.mutation({
+      query: (data) => ({
+        url: `${USERS_URL}/auth`,
+        method: "POST",
+        body: data,
+      }),
+    }),
+  }),
+});
+
+export const { useLoginMutation } = userApiSlice;
